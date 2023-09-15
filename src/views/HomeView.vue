73ccfc63219a29571643fdfec2d8d440a431a518
Oriery <panev291002@gmail.com>
2023-09-15 17:53:39 +0300

    rm files and add some basic styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    This is a Home View
+    <HelloWorld />
   </main>
 </template>
+
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
