d0b17bac7e259e16f6dce0650c7c9eec76141e8e
Oriery <panev291002@gmail.com>
2023-09-15 16:59:28 +0300

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+    <h1 class="text-3xl font-bold underline">
+      This text is styled with underline by Tailwind
+    </h1>
+
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
