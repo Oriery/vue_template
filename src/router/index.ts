8cb9ed3721cddafa297a421372fb49202386e723
Oriery <panev291002@gmail.com>
2023-11-16 16:38:52 +0300

    do not support locale in url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import { getLocale, isLocaleSupported, setI18nLanguage } from '@/i18n'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,26 +24,6 @@ const router = createRouter({
       component: () => import('../views/404View.vue'),
     },
   ],
-})
-
-// navigation guards
-router.beforeEach(async (to) => {
-  const paramsLocale = to.params.locale as string | undefined
-
-  if (!paramsLocale) {
-    return
-  }
-
-  // use locale if paramsLocale is not in SUPPORT_LOCALES
-  if (!isLocaleSupported(paramsLocale)) {
-    return router.push({
-      name: to.name!,
-      params: { ...to.params, locale: getLocale() },
-    })
-  }
-
-  // set i18n language
-  setI18nLanguage(paramsLocale)
 })
 
 export default router
